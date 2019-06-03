f2ba790805ac52a0e139f7540b50b96f62c097ec
avatarpl <msiedlecki5@gmail.com>
2019-06-03 22:07:42 +0200

    Draft of game interface

--- app/src/main/java/com/example/tetris/MainActivity.java
+++ app/src/main/java/com/example/tetris/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.tetris;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -10,18 +14,40 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    boolean vibrating=false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
         getSupportActionBar().hide(); //hide the title bar
         setContentView(R.layout.activity_main);
-        Button exitButton = (Button) findViewById(R.id.exitButton);
         Button controlsButton = (Button) findViewById(R.id.controlsButton);
+        Button exitButton = (Button) findViewById(R.id.exitButton);
+
+        final Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        controlsButton.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (vibrating == false)
+                {
+                    vibrating=true ;
+
+                    vibe.vibrate(100);
+                    vibrating=false;
+                }
+                return false;
+            }
+        });
+        
 
         exitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 finish();
                 System.exit(0);
             }
@@ -31,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+                vibe.vibrate(100);
                 Intent intent=new Intent(MainActivity.this, controls.class);
                 startActivity(intent);
             }
